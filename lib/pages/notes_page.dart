5d497db1d2ead1b17ed0a545f259154b3648dfc5
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-04-28 06:33:38 +0100

    flutter rebuild app

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,4 +1,8 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
+import 'package:notes_app/models/note_database.dart';
+import 'package:provider/provider.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -8,8 +12,49 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  // text controller to access what the user typed
+  final textController = TextEditingController();
+
+  // create a note
+  void createNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          // create button
+          MaterialButton(
+            onPressed: () {
+              //add to db
+              context.read<NoteDatabase>().addNote(textController.text);
+
+              // pop dialog box
+              Navigator.pop(context);
+            },
+            child: const Text('Create'),
+          )
+        ],
+      ),
+    );
+  }
+
+  // read notes
+
+  //upadte a note
+
+  //delete a note
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
